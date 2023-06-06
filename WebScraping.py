90fb65808848b0d21de5657fc6552d138d03b257
aalamshane <aalamshane@gmail.com>
2023-06-06 22:11:35 +0530

    web scrapping using python

--- WebScraping.py
+++ WebScraping.py
@@ -2,41 +2,38 @@ import bs4
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 
-my_url = 'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20card'
+my_url = 'http://books.toscrape.com/'
 
 # opening url and grabbing the web page
 uClient = urlopen(my_url)
-page_html = uClient.read()
+page_html = uClient.read().decode('utf-8')
 uClient.close()
 
 # html parsing
 page_soup = soup(page_html, 'html.parser')
 
-# grabbing all containers with class name = item-container
-containers = page_soup.findAll('div', {'class':'item-container'})
+# grabbing all containers with class name = product_pod
+containers = page_soup.findAll('article', {'class': 'product_pod'})
 
 filename = "products.csv"
 f = open(filename, 'w')
 
-headers = "brands, product_name, shipping\n"
+headers = "title, price, in stock\n"
 
 f.write(headers)
 
 container = containers[1]
 
 for container in containers:
-    brand = container.div.div.a.img['title']
-    title_container = container.findAll('a', {'class':'item-title'})
-    product_name = title_container[0].text
-    ship_container = container.findAll('li', {'class':'price-ship'})
-    # use strip() to remove blank spaces before and after text
-    shipping = ship_container[0].text.strip()
+    # Example: Get the book title
+    title = container.h3.a['title']
+    price_container = container.findAll('p', {'class':'price_color'})[0].text.strip()
+    available_book = container.findAll('p', {'class':'instock availability'})[0].text.strip()
+    print("brand:" + title)
+    print("available_book:" + available_book)
+    print("Price:" + price_container)
 
-    print("brand:" + brand)
-    print("product_name:" + product_name)
-    print("shipping:" + shipping)
-
-    f.write(brand + ',' + product_name.replace(',' , '|') + ',' + shipping + '\n')
+    f.write(title + ',' +price_container + ',' + available_book + '\n')
 
 f.close()
 
